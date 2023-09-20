20ac822f164353bbfd504cdabb18d1fdbcebc23e
Pedro Gaya <pedro.gaya@cubos.academy>
2023-09-20 14:52:24 -0300

    feat: finished nginx proxy with node and mysql

--- app/src/server.js
+++ app/src/server.js
@@ -1,9 +1,39 @@
 const express = require("express");
-
+const mysql = require("mysql");
+const faker = require("faker-br");
 const app = express();
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
 
-app.get("/", (req, res) => {
-  return res.send("<h1>Full cycle</h1>");
+const connection = mysql.createConnection(config);
+connection.connect();
+const sql = `INSERT INTO names (name) VALUES ('${faker.name.firstName()}')`;
+connection.query(sql);
+connection.end();
+
+app.get("/", async (req, res) => {
+  let returnString = "<h1>Full Cycle Rocks!</h1>";
+
+  const connection = mysql.createConnection(config);
+  connection.connect();
+  connection.query("SELECT * FROM names", function (error, results, fields) {
+    if (error) return res.send(returnString);
+    if (results.length) {
+      returnString += "<ul>";
+
+      for (const person of results) {
+        returnString += `<li>${person.name}</li>`;
+      }
+
+      returnString += "</ul>";
+    }
+
+    return res.send(returnString);
+  });
 });
 
 app.listen(3000, () => console.log("listening on port 3000"));
